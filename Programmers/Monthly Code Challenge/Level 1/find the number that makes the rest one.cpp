25e6fc799d06b5f93c3774bfd2807eb378eb2709
Kdelphinus <delphinus@khu.ac.kr>
2021-11-03 18:55:23 +0900

    해결

---
+++ Programmers/Monthly Code Challenge/Level 1/find the number that makes the rest one.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file find the number that makes the rest one.cpp
+ * @author Kdelphinus (delphinus@khu.ac.kr)
+ * @brief Monthly Code Chalange Season 3
+ * @date 2021-11-03 18:54:49
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 1)
+            return i;
+    }
+}
